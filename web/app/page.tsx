c4e09f4912fd2c3c24ab5bcaa12d56024166a813
caleb <calebcarithers@me.com>
2023-12-20 13:04:42 -0600

    map

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,9 +1,13 @@
+import Map from "@/components/Map";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div>
         vibes only
+      </div>
+      <div>
+        <Map />
       </div>
     </main>
   )
