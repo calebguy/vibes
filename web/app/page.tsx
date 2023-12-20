9d67716dd2b799c94a128d5f4cb359a803a5fff0
caleb <calebcarithers@me.com>
2023-12-20 13:41:10 -0600

    map up

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,13 +1,35 @@
+"use client"
+
 import Map from "@/components/Map";
+import { useState } from "react";
+import { ViewState } from "react-map-gl";
 
 export default function Home() {
+  const homeViewState: ViewState = {
+    longitude: -97.7041992,
+    latitude: 30.2796329,
+    zoom: 20,
+    bearing: 0,
+    pitch: 0,
+    padding: {
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+    }
+  }
+  const [view, setView] = useState<ViewState>(homeViewState);
   return (
     <main className="flex min-h-screen flex-col items-center p-12 w-full">
       <div>
         vibes only
       </div>
-      <div className="grow w-full min-h-[1px]">
-        <Map />
+      <div className="grow w-full h-0 relative">
+        <Map view={view} onMove={(e) => setView(e.viewState)}/>
+        <div className="absolute flex justify-between bottom-0 w-full px-8 py-4 text-black">
+          <div>{view.longitude.toPrecision(6)}, {view.latitude.toPrecision(6)} +{view.zoom.toPrecision(6)}</div>
+          <button onClick={() => setView(homeViewState)}>home</button>
+        </div>
       </div>
     </main>
   )
--- web/components/Map.tsx
+++ web/components/Map.tsx
@@ -1,36 +1,18 @@
 "use client"
 
-import { Map as MapboxMap } from "react-map-gl";
+import { Map as MapboxMap, ViewState, ViewStateChangeEvent } from "react-map-gl";
 import env from "@/helpers/env";
-import { useState } from "react";
+interface MapProps {
+  view: ViewState
+  onMove: (evt: ViewStateChangeEvent) => void;
+}
 
-export default function Map() {
-  const [viewState, setViewState] = useState({
-    longitude: -97.7041992,
-    latitude: 30.2796329,
-    zoom: 10
-  });
-
-  return <div className="w-full h-full">
-    <div className="w-full h-full">
-      <MapboxMap 
-        {...viewState}
-        onMove={evt => setViewState(evt.viewState)}
-        mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_TOKEN}
-        style={{ width: "100%", height: "100%" }}
-        mapStyle="mapbox://styles/mapbox/streets-v9"
-      />
-    </div>  
-    <div className="break-words break-all">
-      <div>
-        lat: {viewState.latitude}
-      </div>
-      <div>
-        long: {viewState.longitude}
-      </div>
-      <div>
-        zoom: {viewState.zoom}
-      </div>
-    </div>
-  </div>
+export default function Map({ view, onMove }: MapProps) {
+  return <MapboxMap 
+    {...view}
+    onMove={onMove}
+    mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_TOKEN}
+    style={{ width: "100%", height: "100%" }}
+    mapStyle="mapbox://styles/mapbox/streets-v9"
+  />
 }
