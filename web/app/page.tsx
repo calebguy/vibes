8165654d01fcd46398911276877ee30208ae7277
caleb <calebcarithers@me.com>
2023-12-20 13:18:12 -0600

    up

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -2,11 +2,11 @@ import Map from "@/components/Map";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+    <main className="flex min-h-screen flex-col items-center p-12 w-full">
       <div>
         vibes only
       </div>
-      <div>
+      <div className="grow w-full min-h-[1px]">
         <Map />
       </div>
     </main>
--- web/components/Map.tsx
+++ web/components/Map.tsx
@@ -2,11 +2,35 @@
 
 import { Map as MapboxMap } from "react-map-gl";
 import env from "@/helpers/env";
+import { useState } from "react";
 
 export default function Map() {
-  return <MapboxMap 
-    mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_TOKEN}
-    style={{ width: "100%", height: "100%" }}
-    mapStyle="mapbox://styles/mapbox/streets-v9"
-  />
+  const [viewState, setViewState] = useState({
+    longitude: -97.7041992,
+    latitude: 30.2796329,
+    zoom: 10
+  });
+
+  return <div className="w-full h-full">
+    <div className="w-full h-full">
+      <MapboxMap 
+        {...viewState}
+        onMove={evt => setViewState(evt.viewState)}
+        mapboxAccessToken={env.NEXT_PUBLIC_MAPBOX_TOKEN}
+        style={{ width: "100%", height: "100%" }}
+        mapStyle="mapbox://styles/mapbox/streets-v9"
+      />
+    </div>  
+    <div className="break-words break-all">
+      <div>
+        lat: {viewState.latitude}
+      </div>
+      <div>
+        long: {viewState.longitude}
+      </div>
+      <div>
+        zoom: {viewState.zoom}
+      </div>
+    </div>
+  </div>
 }
